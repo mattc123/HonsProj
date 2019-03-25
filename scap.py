85e4a70e7bd704db6d25f0e2747ed78a4a5387ab
mattc123 <35926868+mattc123@users.noreply.github.com>
2019-03-25 19:41:36 +0000

    reads pcap files and produces meaningful hex

--- scap.py
+++ scap.py
@@ -1,15 +1,24 @@
-from scapy.all import sniff, wrpcap, hexdump
+from scapy.all import sniff, wrpcap, hexdump, rdpcap
 
 
-pkts_list = sniff(count=1, monitor=False)#set monitor=True
+#pkts_list = sniff(count = 5, monitor=False)#set monitor=True
 
-pkts_list.show()
-
-
-
+#pkts_list.show()
 #hexdump(pkts_list)
-
-
-
 #wrpcap('scapytest.pcap',pkts_list )
-hexdump(pkts_list[0])
+
+#for i in pkts_list:
+#	hexdump(i)
+
+
+
+packets = rdpcap('test1.pcapng')
+
+for i in packets:
+	hexdump(i)
+	
+	
+
+	
+	
+	
